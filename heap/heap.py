e1c943b25117b861de7d45a8c444d2934f1269c9
Peter Pham <phamhere@gmail.com>
2019-02-25 14:39:34 -0700

    finished heap implementation

--- heap/heap.py
+++ heap/heap.py
@@ -1,21 +1,47 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        self.storage.append(value)
+        self._bubble_up(len(self.storage) - 1)
 
-  def delete(self):
-    pass
+    def delete(self):
+        deleted = self.storage.pop(0)
+        self._sift_down(0)
+        return deleted
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        return self.storage[0]
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        return len(self.storage)
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        # keeps looping until the top of the heap is reached
+        while (index - 1) // 2 >= 0:
+            # if the value of the parent is less than the child, swap
+            if self.storage[(index - 1) // 2] < self.storage[index]:
+                self.storage[(
+                    index - 1) // 2], self.storage[index] = self.storage[index], self.storage[(index - 1) // 2]
+            # decrementing index so loop will start at parent's parent
+            index = (index - 1) // 2
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        # checks to see if a child element exists in self.storage
+        while index * 2 + 1 < len(self.storage):
+            # if the second child doesn't exist, by default the first child is biggest
+            if index * 2 + 2 > len(self.storage) - 1:
+                biggest_child = index * 2 + 1
+            # compares the children to see which is biggest
+            else:
+                if self.storage[index * 2 + 1] > self.storage[index * 2 + 2]:
+                    biggest_child = index * 2 + 1
+                else:
+                    biggest_child = index * 2 + 2
+            # compares the parent to the biggest child, swapping if the parent is less
+            if self.storage[index] < self.storage[biggest_child]:
+                self.storage[index], self.storage[biggest_child] = self.storage[biggest_child], self.storage[index]
+            # changes index to be child for next iteration
+            index = biggest_child
+
